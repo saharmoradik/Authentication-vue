7b886c06e9d204222b11cdf5deedb63e935900d2
saharmoradik <saharmoradik@gmail.com>
2022-10-11 12:58:26 +0200

    NavBar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="bg-gray-100 fixed inset-x-0">
+    <div class="max-w-6xl mx-auto px-4">
+      <div class="flex justify-between">
+        <div class="flex space-x-4">
+          <!-- logo -->
+          <div>
+            <a
+              href="#"
+              class="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
+            >
+              <i class="bx bxl-medium-old mr-1 text-xl mb-1 text-blue-400"></i>
+              <span class="font-bold">App</span>
+            </a>
+          </div>
+          <!-- primary nav -->
+          <div class="hidden md:flex items-center space-x-1">
+            <a href="#" class="py-5 px-3 text-gray-700 hover:text-gray-900"
+              >Home</a
+            >
+          </div>
+        </div>
+        <!-- secondary nav -->
+        <div class="hidden md:flex items-center space-x-1">
+          <a href="" class="py-5 px-3">Login</a>
+          <a
+            href=""
+            class="py-2 px-3 bg-yellow-400 text-white hover:bg-yellow-300 text-sm hover:text-yellow-800 rounded transition duration-300"
+            >Signup</a
+          >
+        </div>
+        <!-- mobile button goes here -->
+        <div class="md:hidden flex items-center">
+          <button class="mobile-menu-button focus:outline-none">
+            <i class="bx bx-menu text-3xl mt-1"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <!-- mobile menu -->
+    <div class="mobile-menu hidden md:hidden">
+      <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Home</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
